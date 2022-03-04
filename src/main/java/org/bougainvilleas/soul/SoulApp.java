28a1003ed8b82a9f7ae55e340bfdef2cb935fc1c
caddy <caddyRen@qq.com>
2022-03-04 20:25:27 +0800

    feat: init commit

--- src/main/java/org/bougainvilleas/soul/SoulApp.java
+++ src/main/java/org/bougainvilleas/soul/SoulApp.java
@@ -8,20 +8,39 @@ import javafx.stage.Stage;
 
 public class SoulApp extends Application
 {
+
+    public static int add(int x, int y)
+    {
+        return x + y;
+    }
+
     public static void main(String[] args)
     {
-        System.err.println("2333");
+        int result = add(5, 6);
+        System.err.println("x+y= " + result);
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception
     {
-        Circle circle=new Circle(40,40,30);
+        Circle circle = new Circle(40, 40, 30);
         Group group = new Group(circle);
         Scene scene = new Scene(group, 400, 300);
-        primaryStage.setTitle("陈彩灵");
+        primaryStage.setTitle("嘿嘿");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+
+//    public static void main(String[] args)
+//    {
+//        Application.launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception
+//    {
+//        primaryStage.setTitle("sos");
+//        primaryStage.show();
+//    }
 }
